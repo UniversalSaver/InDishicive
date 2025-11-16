a5cf306416df00ae4684d6c30b8263a5f74f2186
MZH006 <hashmi212411@gmail.com>
2025-11-16 14:29:58 -0500

    Small fix based on tests

--- src/main/java/interface_adapter/view_favorite/ViewFavoriteViewModel.java
+++ src/main/java/interface_adapter/view_favorite/ViewFavoriteViewModel.java
@@ -1,18 +1,17 @@
 package interface_adapter.view_favorite;
 
-import interface_adapter.ViewModel;
-import entity.Recipe;
+import java.util.ArrayList;
 import java.util.List;
+import entity.Recipe;
+import interface_adapter.ViewModel;
 
-/**
- * ViewModel for the View Favorites use case.
- * Follows the same pattern as ViewRecipesViewModel.
- */
+
 public class ViewFavoriteViewModel extends ViewModel<List<Recipe>> {
 
     public static final String FAV_LOADED = "favorites_loaded";
 
     public ViewFavoriteViewModel() {
         super("view_favorites");
+        setState(new ArrayList<>());
     }
 }
