00a897fc5e24047c987396b660db3291fd09d0e5
Nabob <nabhan.rashid@student.tdsb.on.ca>
2025-11-16 21:03:40 -0500

    Added the Ingredient Edit panel. The Add Ingredient button will work upon these.

--- src/main/java/view/user_recipe_view/IngredientChoice.java
+++ src/main/java/view/user_recipe_view/IngredientChoice.java
@@ -1,6 +1,55 @@
 package view.user_recipe_view;
 
 import javax.swing.*;
+import java.util.List;
 
 public class IngredientChoice extends JPanel {
+
+	private String ingredientName;
+	private String ingredientAmount;
+
+	private JComboBox<String> ingredientComboBox;
+	private JTextField amountField = new JTextField();
+
+	public String getIngredientName() {
+		return ingredientName;
+	}
+
+	public String getIngredientAmount() {
+		return ingredientAmount;
+	}
+
+	public IngredientChoice(List<String> possibleIngredients) {
+		GroupLayout layout = new GroupLayout(this);
+		layout.setAutoCreateGaps(true);
+
+
+		ingredientComboBox = new JComboBox(possibleIngredients.toArray());
+		ingredientComboBox.setEditable(true);
+
+		JLabel nameLabel = new JLabel("Name:");
+		JLabel amountLabel = new JLabel("Amount:");
+
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup()
+						.addComponent(nameLabel)
+						.addComponent(amountLabel)
+				)
+				.addGroup(layout.createParallelGroup()
+						.addComponent(ingredientComboBox)
+						.addComponent(amountField)
+				)
+		);
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup()
+						.addComponent(nameLabel)
+						.addComponent(ingredientComboBox)
+				)
+				.addGroup(layout.createParallelGroup()
+						.addComponent(amountLabel)
+						.addComponent(amountField)
+				)
+		);
+
+	}
 }
