78ba619a5a1060dc3331d81d16cc0f3bb8d7bace
Nabob <nabhan.rashid@student.tdsb.on.ca>
2025-11-22 18:15:49 -0500

    Added a final test to AddRecipeInteractorTest to ensure correct logic in DAO failure.

--- src/test/use_cases/add_recipe/AddRecipeInteractorTest.java
+++ src/test/use_cases/add_recipe/AddRecipeInteractorTest.java
@@ -1,6 +1,5 @@
 package use_cases.add_recipe;
 
-import entity.UserRecipe;
 import org.junit.jupiter.api.Test;
 import use_case.add_recipe.*;
 
@@ -34,6 +33,35 @@ public class AddRecipeInteractorTest {
 			}
 		};
 		AddRecipeDataAccessInterface dataAccess = recipe -> AddRecipeDataAccessInterface.ADDED_MESSAGE;
+
+		AddRecipeInteractor addRecipeInteractor = new AddRecipeInteractor(dataAccess, outputBoundary);
+
+		addRecipeInteractor.execute(inputData);
+	}
+
+	/**
+	 * A test where the DAO decided something is wrong with the recipe.
+	 */
+	@Test
+	void DAOFailureTest() {
+		List<String> ingredientNames = Arrays.stream(new String[] {"Milk", "Unsweetened cocoa powder",
+				"Granulated sugar", "Chocolate chips", "Vanilla Extract"}).toList();
+		List<String> ingredientAmounts = Arrays.stream(new String[] {"4 cups", "1/4 cup", "1/4 cup", "1/2 cup",
+				"1/4 tsp"}).toList();
+		AddRecipeInputData inputData = new AddRecipeInputData(ingredientNames, ingredientAmounts, "Hot Chocolate",
+				"A cool hot chocolate recipe", "Place milk, cocoa powder and sugar in a small saucepan. Heat over medium/medium-low heat, whisking frequently, until warm (but not boiling). Add chocolate chips and whisk constantly until the chocolate chips melt and distribute evenly into the milk. Whisk in vanilla extract and a pinch of salt (if desired), serve immediately. ");
+		AddRecipeOutputBoundary outputBoundary = new AddRecipeOutputBoundary() {
+			@Override
+			public void prepareSuccessView() {
+				fail("Should not have succeeded.");
+			}
+
+			@Override
+			public void prepareFailureView(String message) {
+				assertEquals("Something went wrong.", message);
+			}
+		};
+		AddRecipeDataAccessInterface dataAccess = recipe -> "Something went wrong.";
 
 		AddRecipeInteractor addRecipeInteractor = new AddRecipeInteractor(dataAccess, outputBoundary);
 
