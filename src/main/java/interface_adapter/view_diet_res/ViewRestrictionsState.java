2019c4d87409df0576afd3a33d5e31a73a96f40c
Mert Rassa <mertrassa@gmail.com>
2025-11-22 21:05:30 -0500

    View Models for Dietary Restrictions are updated.

--- src/main/java/interface_adapter/view_diet_res/ViewRestrictionsState.java
+++ src/main/java/interface_adapter/view_diet_res/ViewRestrictionsState.java
@@ -1,4 +1,32 @@
 package interface_adapter.view_diet_res;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewRestrictionsState {
+    private List<String> restrictions = new ArrayList<>();
+    private String error = null;
+
+    public ViewRestrictionsState(ViewRestrictionsState copy) {
+        this.restrictions = copy.restrictions;
+        this.error = copy.error;
+    }
+
+    public ViewRestrictionsState() {}
+
+    public List<String> getRestrictions() {
+        return restrictions;
+    }
+
+    public void setRestrictions(List<String> restrictions) {
+        this.restrictions = restrictions;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
