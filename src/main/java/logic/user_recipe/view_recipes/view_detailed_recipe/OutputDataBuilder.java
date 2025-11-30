dadfb93d8173f91b9d19f0b7509685b5ddd615ad
Nabhan Rashid <nabhanbd06@gmail.com>
2025-11-29 23:12:28 -0500

    Implemented logic for the view recipe class. Now the presenter and the view should be implemented.

---
+++ src/main/java/logic/user_recipe/view_recipes/view_detailed_recipe/OutputDataBuilder.java
@@ -0,0 +1,53 @@
+package logic.user_recipe.view_recipes.view_detailed_recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Ingredient;
+
+/**
+ * A builder for the output data, because I find the creation to be ugly otherwise.
+ */
+public class OutputDataBuilder {
+    private List<String> ingredientAmounts = new ArrayList<>();
+    private List<String> ingredientNames = new ArrayList<>();
+    private String title;
+    private String steps;
+
+    /**
+     * Adds a title to the builder.
+     * @param givenTitle title to be given
+     * @return the builder
+     */
+    OutputDataBuilder addTitle(String givenTitle) {
+        title = givenTitle;
+        return this;
+    }
+
+    /**
+     * Adds an ingredient to the builder. This guarantees the order is maintained.
+     * @param ingredient ingredient to be added
+     * @return the builder
+     */
+    OutputDataBuilder addIngredient(Ingredient ingredient) {
+        ingredientNames.add(ingredient.getName());
+        ingredientAmounts.add(ingredient.getAmount());
+
+        return this;
+    }
+
+    /**
+     * Adds the steps to the builder.
+     * @param givenSteps steps to be added
+     * @return the builder
+     */
+    OutputDataBuilder addSteps(String givenSteps) {
+        steps = givenSteps;
+
+        return this;
+    }
+
+    ViewUserRecipeOutputData build() {
+        return new ViewUserRecipeOutputData(title, steps, ingredientNames, ingredientAmounts);
+    }
+}
---
+++ src/main/java/logic/user_recipe/view_recipes/view_detailed_recipe/ViewUserRecipeDetailsInteractor.java
@@ -0,0 +1,43 @@
+package logic.user_recipe.view_recipes.view_detailed_recipe;
+
+import entity.Ingredient;
+import entity.UserRecipe;
+
+/**
+ * A concrete interactor.
+ * Finds the user recipe given a title, then calls a presenter to show it to the user.
+ */
+public class ViewUserRecipeDetailsInteractor implements ViewUserRecipeDetailsInputBoundary {
+    private final ViewUserRecipeDetailsDataAccessInterface viewUserRecipeDetailsDataAccess;
+    private final ViewUserRecipeDetailsOutputBoundary viewUserRecipeDetailsPresenter;
+
+    public ViewUserRecipeDetailsInteractor(ViewUserRecipeDetailsDataAccessInterface viewUserRecipeDetailsDataAccess,
+                                           ViewUserRecipeDetailsOutputBoundary viewUserRecipeDetailsPresenter) {
+        this.viewUserRecipeDetailsDataAccess = viewUserRecipeDetailsDataAccess;
+        this.viewUserRecipeDetailsPresenter = viewUserRecipeDetailsPresenter;
+    }
+
+    @Override
+    public void execute(ViewUserRecipeDetailsInputData inputData) {
+        final UserRecipe gottenRecipe = viewUserRecipeDetailsDataAccess.getRecipeByTitle(inputData.getTitle());
+
+        if (gottenRecipe == null) {
+            viewUserRecipeDetailsPresenter.presentFailureView();
+        } else {
+            viewUserRecipeDetailsPresenter.presentSuccessView(createOutputData(gottenRecipe));
+        }
+    }
+
+    private static ViewUserRecipeOutputData createOutputData(UserRecipe gottenRecipe) {
+        final OutputDataBuilder builder = new OutputDataBuilder();
+
+        builder.addTitle(gottenRecipe.getTitle());
+        builder.addSteps(gottenRecipe.getSteps());
+
+        for (Ingredient ingredient : gottenRecipe.getIngredients()) {
+            builder.addIngredient(ingredient);
+        }
+
+        return builder.build();
+    }
+}
